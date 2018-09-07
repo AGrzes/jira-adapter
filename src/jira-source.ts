a76960ce91ffbed7d6fd8de0592a2bd0aee2ebb0
Grzegorz Abramczyk <abramczykg@gmail.com>
2018-09-07 16:09:24 +0200

    First jira source test

---
+++ src/jira-source.ts
@@ -0,0 +1,10 @@
+
+import axios, { AxiosInstance } from 'axios'
+import {from, Observable} from 'rxjs'
+export class JiraClient {
+  constructor(private client: AxiosInstance) {}
+
+  public all(): Observable<any> {
+    return from(this.client.post('/rest/api/2/search').then((response) => response.data))
+  }
+}
---
+++ test/jira-source.ts
@@ -0,0 +1,27 @@
+import axios from 'axios'
+import {expect} from 'chai'
+import 'mocha'
+import * as moxios from 'moxios'
+import {JiraClient} from '../src/jira-source'
+
+describe('JiraClient', function() {
+  beforeEach(function() {
+    // import and pass your custom axios instance to this method
+    moxios.install()
+  })
+
+  afterEach(function() {
+    // import and pass your custom axios instance to this method
+    moxios.uninstall()
+  })
+  describe('all', function() {
+    const client = new JiraClient(axios)
+    it('should call jira search', function() {
+      moxios.stubRequest('/rest/api/2/search', {response:{}})
+      return client.all().forEach(() => {
+        const request = moxios.requests.get('POST', '/rest/api/2/search')
+        expect(request).to.exist
+      })
+    })
+  })
+})
