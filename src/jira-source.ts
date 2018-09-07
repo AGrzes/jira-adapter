ecb51f83693709e3947d32f2a8986be29dc9825b
Grzegorz Abramczyk <abramczykg@gmail.com>
2018-09-07 21:25:30 +0200

    Handling multiple pages

--- src/jira-source.ts
+++ src/jira-source.ts
@@ -1,12 +1,21 @@
 
 import axios, { AxiosInstance } from 'axios'
-import {from, Observable, of} from 'rxjs'
+import {from, Observable, Observer, of} from 'rxjs'
 import {flatMap} from 'rxjs/operators'
 export class JiraClient {
   constructor(private client: AxiosInstance) {}
 
   public all(): Observable<any> {
-    return from(this.client.post('/rest/api/2/search', {jql: '', fields: ['*all']}))
-      .pipe(flatMap((response) => of(...(response.data.issues || []))))
+    return Observable.create((observer: Observer<any>) => {
+      const nextPage = () => this.client.post('/rest/api/2/search', {jql: '', fields: ['*all']}).then((response) => {
+        if (response.data.issues) {
+          response.data.issues.forEach((issue) => observer.next(issue))
+          nextPage()
+        } else {
+          observer.complete()
+        }
+      })
+      nextPage()
+    })
   }
 }
--- test/jira-source.ts
+++ test/jira-source.ts
@@ -5,6 +5,17 @@ import * as moxios from 'moxios'
 import {toArray} from 'rxjs/operators'
 import {JiraClient} from '../src/jira-source'
 
+function responses(...requests: any[]) {
+  requests.reverse()
+  const wait = () => moxios.wait(() => {
+      moxios.requests.mostRecent().respondWith(requests.pop())
+      if (requests.length) {
+        wait()
+      }
+    }, 1)
+  wait()
+}
+
 describe('JiraClient', function() {
   beforeEach(function() {
     // import and pass your custom axios instance to this method
@@ -18,28 +29,37 @@ describe('JiraClient', function() {
   describe('all', function() {
     const client = new JiraClient(axios)
     it('should call jira search', function() {
-      moxios.stubRequest('/rest/api/2/search', {response: {}})
+      responses({response: { }})
       return client.all().forEach(() => {
         const request = moxios.requests.get('POST', '/rest/api/2/search')
         expect(request).to.exist
       })
     })
     it('should send jql', function() {
-      moxios.stubRequest('/rest/api/2/search', {response: {}})
+      responses({response: { }})
       return client.all().forEach(() => {
         const request = moxios.requests.get('POST', '/rest/api/2/search')
         expect(JSON.parse(request.config.data)).to.have.property('jql').equals('')
       })
     })
     it('should send fields', function() {
-      moxios.stubRequest('/rest/api/2/search', {response: {}})
+      responses({response: { }})
       return client.all().forEach(() => {
         const request = moxios.requests.get('POST', '/rest/api/2/search')
         expect(JSON.parse(request.config.data)).to.have.property('fields').deep.equals(['*all'])
       })
     })
     it('should return issues', function(done) {
-      moxios.stubRequest('/rest/api/2/search', {response: { issues: ['a', 'b', 'c']}})
+      responses({response: { issues: ['a', 'b', 'c']}}, {response: { }})
+      client.all().pipe(toArray()).subscribe({
+        next(result) {
+          expect(result).to.deep.equals(['a', 'b', 'c'])
+          done()
+        }
+      })
+    })
+    it('should combine pages', function(done) {
+      responses({response: { issues: ['a', 'b']}}, {response: { issues: ['c']}}, {response: { }})
       client.all().pipe(toArray()).subscribe({
         next(result) {
           expect(result).to.deep.equals(['a', 'b', 'c'])
